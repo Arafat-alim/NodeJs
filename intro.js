3a326e4395cf37c28953c186e12b31b923c0fa38
Arafat <arafat97alim@gmail.com>
2021-12-13 01:16:30 +0530

    intro of node js done.

--- intro.js
+++ intro.js
@@ -11,16 +11,15 @@ console.log(os.type()); // Windows_NT
 console.log(os.version()); // Windows 10 Pro
 console.log(os.homedir()); // C:\Users\barfs
 
-// console.log(__dirname); //E:\ARAFAT\NodeTuGaya
-// console.log(__filename); //E:\ARAFAT\NodeTuGaya\intro.js
+console.log(__dirname); //E:\ARAFAT\NodeTuGaya
+console.log(__filename); //E:\ARAFAT\NodeTuGaya\intro.js
 
-// console.log(path);
+console.log(path);
 
-/*console.log(path.dirname(__filename)); //E:\ARAFAT\NodeTuGaya
+console.log(path.dirname(__filename)); //E:\ARAFAT\NodeTuGaya
 console.log(path.basename(__filename)); //intro.js
 console.log(path.extname(__filename)); //.js
 console.log(path.parse(__filename));
-*/
 
 // math
 console.log(math.add(2, 3));
